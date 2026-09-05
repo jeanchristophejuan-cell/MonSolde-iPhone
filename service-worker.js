390cd3250aecac44e2f84bf8b37e6d1e2d68c6bf
jeanchristophejuan-cell <jeanchristophejuan@gmail.com>
2026-09-05 15:51:16 +0200

    Finalize offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE = "mon-solde-v1";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"];
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
